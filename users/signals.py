feb8554310e1231f9f6d538435d716fb59daac04
Ramil Albakov <ramil.albakov2@gmail.com>
2024-01-04 15:13:42 +0600

    fix: add new user to Regular Users group when group is already exists

--- users/signals.py
+++ users/signals.py
@@ -7,8 +7,9 @@ from django.dispatch import receiver
 def add_user_to_group(sender, instance, created, **kwargs):
     """Add a new user to the 'Regular Users' group upon user creation."""
     if created:
-        common_users, is_created = Group.objects.get_or_create(
-            name='Regular Users',
-        )
-        if is_created:
-            instance.groups.add(common_users)
+        group_name = 'Regular Users'
+        try:
+            regular_users_group = Group.objects.get(name=group_name)
+        except Group.DoesNotExist:
+            regular_users_group = Group.objects.create(name=group_name)
+        instance.groups.add(regular_users_group)
