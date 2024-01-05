c91c7ed871d3c781062bf8558adaa2093f24f865
Ramil Albakov <ramil.albakov2@gmail.com>
2024-01-05 10:17:49 +0600

    Revert "build: replace python-dotenv with django-dotenv"
    This reverts commit 14dda2a59539df5b4f8978f466e4247f5d7feed9.

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,14 +8,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import read_dotenv
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-read_dotenv(BASE_DIR / '.env', override=True)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
