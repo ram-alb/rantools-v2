6633e421c77c64445e796e6bf27c88f96a54bcd1
Ramil Albakov <ramil.albakov2@gmail.com>
2024-07-15 11:12:51 +0500

    feat: add custom filter for django templates

--- config/settings.py
+++ config/settings.py
@@ -69,6 +69,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'custom_filters': 'config.templatetags.custom_filters',
+            },
         },
     },
 ]
