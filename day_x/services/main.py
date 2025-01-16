da5ee4a1c9911ae3019ce2fab2b2ae22cc4fa3c8
Ramil Albakov <ramil.albakov2@gmail.com>
2025-01-16 15:05:09 +0500

    feat: dayX mail list is taken from DayX group

--- day_x/services/main.py
+++ day_x/services/main.py
@@ -1,13 +1,15 @@
-from network_live import update_nl, update_zte_gsm
+from django.contrib.auth.models import Group
 from send_mail import send_email
 
 from day_x.services.reports import fill_excel
 from day_x.services.sql import get_network_live_data
+from network_live import update_nl
 
 
 def send_dayx_file() -> None:
+    """Send dayX file to emails."""
     # update Network Live data
-    # update_results = update_zte_gsm()
+    update_results = update_nl()
 
     # get updated data
     nl_data = get_network_live_data()
@@ -15,8 +17,11 @@ def send_dayx_file() -> None:
     # fill excell
     report_path = fill_excel(nl_data)
 
+    # get email list of users in DayX group
+    dayx_group = Group.objects.get(name='DayX')
+    to = dayx_group.user_set.values_list("email", flat=True)
+
     # send report
-    # to = 'Ramil.Albakov@kcell.kz'
-    # subject = 'DayX file'
-    # message = '\n'.join(update_results)
-    # send_email(to, subject, message, filepaths=report_path)
+    subject = 'DayX file'
+    message = '\n'.join(update_results)
+    send_email(list(to), subject, message, filepaths=report_path)
