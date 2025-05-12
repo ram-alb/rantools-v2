70818837fa354a4ccf8f0b8f54a9b78acde036d0
Ramil Albakov <ramil.albakov2@gmail.com>
2025-05-12 11:43:12 +0500

    feat: lte diff sorted by subnetworks

--- network_vs_atoll/services/lte/main.py
+++ network_vs_atoll/services/lte/main.py
@@ -20,7 +20,10 @@ def main():
         if diff:
             diffs.setdefault(subnetwork, []).extend(diff)
 
-    diff_count_by_subnetwork = {subnetwork: len(diffs) for subnetwork, diffs in diffs.items()}
+    diff_count_by_subnetwork = dict(sorted(
+        ((subnetwork, len(diff)) for subnetwork, diff in diffs.items()),
+        key=lambda subnetwork_diff: subnetwork_diff[0],
+    ))
     total_diff_count = sum(diff_count_by_subnetwork.values())
 
     return total_diff_count, diff_count_by_subnetwork, diffs
